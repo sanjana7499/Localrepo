a9cfabc8c05f2352e2cb3e12f0f143f6f195e8e2
sanjana7499 <sanjansaraut0110@gmail.com>
2025-07-22 00:33:07 +0530

    f2

--- simple.java
+++ simple.java
@@ -2,6 +2,6 @@ package Localrepo;
 
 class simple {
     public static void main(String[] args) {
-        System.out.println("welcome to sanjana sachin raut club");
+        System.out.println("welcome to sachin raut club");
     }
 }
